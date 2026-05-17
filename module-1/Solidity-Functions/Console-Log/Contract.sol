d05ffc5f0dc2ec67891ba8d09d6785bec58fc9f8
Biniyam Getachew <binigetachew18@gmail.com>
2026-05-17 16:04:04 +0300

    chore: remove unused configuration file

--- module-1/Solidity-Functions/Console-Log/Contract.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-
-    function winningNumber(string memory) public pure returns (uint256) {
-        return 794;
-    }
-}
--- module-1/Solidity-Functions/Pure-Double/Contract.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-
-    function double(uint256 x) public pure returns (uint256) {
-        return x * 2;
-    }
-}
